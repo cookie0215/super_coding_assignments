35fb9f49526d1b21277eb9d41a63fc882f9e661c
cookie0215 <jina_0215@naver.com>
2023-12-25 14:57:06 +0900

    Fix: 폴더 수정

--- 4주차/DAY5/W4D5.js
+++
@@ -1,7 +0,0 @@
-const bills = [1, 50, 22, 97, 62, 15, 28, 34, 77, 100];
-const calculateTip = bills.map((num) => {
-  const tips = num >= 30 ? parseInt(num * 0.15) : parseInt(num * 0.2);
-  console.log(tips);
-})
-
-calculateTip;
